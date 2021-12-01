cb64f2325b8cebcf3c41f93bab032dc7a11adbd3
timokluser-dev <timo@timokluser.ch>
2021-12-01 20:07:58 +0100

    add 01 December

--- 01/script.js
+++ 01/script.js
@@ -2,33 +2,27 @@ const fs = require('fs');
 
 function getNumbers() {
   return fs
-    .readFileSync('data.txt')
+    .readFileSync(`${__dirname}/data.txt`)
     .toString()
     .split('\n')
-    .filter(val => val.length > 0)
+    .filter(value => value.length > 0)
     .map(Number);
 }
 
 function main() {
   const numbers = getNumbers();
 
-  let previousNumber;
-  let numbersIncreased = 0;
-  let numbersDecreased = 0;
-  for (const number of numbers) {
-    if (previousNumber !== undefined) {
-      const numberHasIncreased = number > previousNumber;
-      (numberHasIncreased && numbersIncreased++) || (!numberHasIncreased && numbersDecreased++);
-      console.log(`${number} ${numberHasIncreased ? '(increased)' : '(decreased)'}`);
-    } else {
-      console.log(`${number} (N/A - no previous measurement)`);
-    }
+  console.log('\n--- Part 1 ---');
+  const measurementsIncreased = numbers.filter((value, i) => numbers[i] < numbers[i + 1]);
+  console.log(`Measurements Increased: ${measurementsIncreased.length}`);
 
-    previousNumber = number;
-  }
-
-  console.log(`\nNumbers Increased: ${numbersIncreased}`);
-  console.log(`Numbers Decreased: ${numbersDecreased}`);
+  console.log('\n--- Part 2 ---');
+  const measurementWindowsIncreased = numbers.filter(
+    (value, i) =>
+      numbers[i] + numbers[i + 1] + numbers[i + 2] <
+      numbers[i + 1] + numbers[i + 2] + numbers[i + 3]
+  );
+  console.log(`Measurement Windows Increased: ${measurementWindowsIncreased.length}`);
 }
 
 main();
